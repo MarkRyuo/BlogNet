47c750dea1089d4d282e2f23be4827772335b884
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 21:14:42 +0800

    Subject : Body

--- client/src/components/Bloglist.jsx
+++ client/src/components/Bloglist.jsx
@@ -4,7 +4,14 @@ export const Bloglist = () => {
 
     return (
         <>
-            
+            <div className={MainContent.blogContainer}>
+                {blogs.map((blog) => (
+                    <div className={MainContent.blogContent} key={blog.id}>
+                        <h1>Title: {blog.title}</h1>
+                            <p>Written by: {blog.author}</p>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,14 +17,7 @@ export const Home = () => {
     return (
         <>
             <div className={MainContent.MainContainer}>
-                <div className={MainContent.blogContainer}>
-                    {blogs.map((blog) => (
-                        <div className={MainContent.blogContent} key={blog.id}>
-                            <h1>Title: {blog.title}</h1>
-                            <p>Written by: {blog.author}</p>
-                        </div>
-                    ))}
-                </div>
+                
             </div>
         </>
     );
