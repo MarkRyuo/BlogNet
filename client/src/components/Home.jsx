1a4c9a765ee491abe6ddda4ab435b36bfa7112ec
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 20:34:13 +0800

    Subject : Body

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
 
     const [blogs, setBlogs] = useState([
         {title: "Beyond to the Infinite", author: "Moda", id: "1"},
-        {title: "Beyond to the Infinite", author: "Niyari", id: "2"},
+        {title: "Beyond to the Infinite: Moon Arc", author: "Niyari", id: "2"},
         {title: "Beyond to the Infinite", author: "Riyuo", id: "3"}
     ])
 
