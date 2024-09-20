b88b3bea1b4706f0c9bd2e2ce5162321a41fda15
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 20:13:15 +0800

    Subject : Body

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,16 +9,6 @@ import the module: e.g import MainContent from './Home.module.css' ;
 
 */
 export const Home = () => {
-
-    const [repost, setRepost] = useState("Jhon Mark Malupa") ;
-    const [post, setPost] = useState("Hello World") ;
-
-    const handleRepost = () => {
-        setRepost("Moda Official")
-        setPost("Hello World reposted by: ")
-    }
-    
-    const Title = 'Homepage' ;
     
     return (
         <>
