a264fc8a093918985ba9ede687d31f9dfc6c3a0b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 09:02:17 +0800

    Subject : Body

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
             <h1>BlogNet</h1>
             <div className="nav-link">
                 <a href="#">Home</a>
-                <a href="#">New Blog</a>
+                <a href="/Create">New Blog</a>
             </div>
         </nav>
     )
