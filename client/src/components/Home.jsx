defbff7773e7f7aec22be7bf7a67a4fc18410222
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 10:17:29 +0800

    Subject : Body

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -18,9 +18,11 @@ export const Home = () => {
     return (
         <main className={MainContent.Main}>
             <div className={MainContent.MainContent}>
-                <h1 className={MainContent.Title}>{Title}</h1>
-                <a href={Link}>Google</a>
-                <p>{Likes}</p>
+                <div>
+                    <h1 className={MainContent.Title}>{Title}</h1>
+                    <a href={Link}>Google</a>
+                    <p>{Likes}</p>
+                </div>
             </div>
         </main>
     );
