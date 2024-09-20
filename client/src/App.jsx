8e687dfcc73a5360b84b0fcba0032e8c906b1200
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 08:04:03 +0800

    Subject : Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,7 @@ function App() {
 
   const Title = 'New Blog' ;
   const Likes = '90 Likes' ;
-  const Person = {
-    name: 'Moda',
-    age: 19
-  }
+  //*const Person = { name: 'Moda', age: 19 }
   const GoogleSite = 'https://www.google.com/' ;
 
 
