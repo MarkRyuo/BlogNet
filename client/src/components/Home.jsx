28036d962bcb7e55dd3c01d7e203ac856f84b822
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 20:13:53 +0800

    Subject : Body

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,9 @@
 import MainContent from './Home.module.css' ;
 import { useState } from 'react';
 
-//* Css Module 
-/* 
 
-Create a Css Module : e.g Home.module.css 
-import the module: e.g import MainContent from './Home.module.css' ;
+//* Use Map()
 
-*/
 export const Home = () => {
     
     return (
