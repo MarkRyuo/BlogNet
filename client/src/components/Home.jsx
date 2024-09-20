685884aba1b9c12f8dab1859d51100fb1c1514eb
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 19:50:12 +0800

    Subject : Body

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -13,7 +13,7 @@ export const Home = () => {
     const [repost, setRepost] = useState("Jhon Mark Malupa") ;
 
     const handleRepost = () => {
-
+        setRepost("Moda Official")
     }
     
     const Title = 'Homepage' ;
@@ -28,7 +28,7 @@ export const Home = () => {
                         <img src='https://i.pinimg.com/originals/c2/fc/1c/c2fc1c0469a53d9d5a9d53e56fb96170.jpg' alt='Error Image' className='' style={{width: "100%", borderRadius: "100px", maxWidth: "100px"}} />
                     </div>
                     <div className={MainContent.blogCredential}>
-                        <a href='' style={{textDecoration: "none", fontSize: "1.5rem"}}>{fname}</a> 
+                        <a href='' style={{textDecoration: "none", fontSize: "1.5rem"}}>{repost}</a> 
                         <p>Post: Hello World</p>
                         <button type='button' onClick={() => {handleRepost}}>Repost</button>
                     </div>
