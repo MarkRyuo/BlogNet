107bb2b7dac886557445fc26ff2e4917cf6508b9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 09:55:03 +0800

    Subject : Body

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,7 @@ import MainContent from './Home.module.css' ;
 /* 
 
 Create a Css Module : e.g Home.module.css 
+import the module: e.g import MainContent from './Home.module.css' ;
 
 */
 export const Home = () => {
