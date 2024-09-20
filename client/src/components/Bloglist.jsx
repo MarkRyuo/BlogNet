2fbf59c4775bea58f39f7f7de71f707e46e39b65
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-20 21:28:59 +0800

    Subject : Body

--- client/src/components/Bloglist.jsx
+++ client/src/components/Bloglist.jsx
@@ -1,12 +1,6 @@
 import MainContent from './Home.module.css' ;
 
 
-interface Propsbloglist {
-    blogs: string ;
-    title: string ;
-}
-
-
 export const Bloglist = (props) => {
 
     const blogs = props.blogs ;
